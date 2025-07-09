ae17c1a92c8f7147365b654794c41791ae335904
Mykhailo Pashnyov <mpashnyov@gmail.com>
2025-07-09 15:22:20 +0200

    Add: spm package

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SchedJoulesApiClient",
+    platforms: [.iOS(.v14), .macCatalyst(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SchedJoulesApiClient",
+            targets: ["ApiClient"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ApiClient",
+            path: "ApiClient/Source",
+        ),
+        .testTarget(
+            name: "ApiClientTest",
+            dependencies: ["ApiClient"],
+            path: "ApiClient/Tests",
+            resources: [.copy("Sample JSONs/")]
+        ),
+    ],
+    swiftLanguageModes: [.v5]
+)
